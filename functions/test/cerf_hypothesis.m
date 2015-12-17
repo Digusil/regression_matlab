b622ccd5e082c09834f76514c155059207b92d54
vy03ravo <ninherrmann@web.de>
2015-12-17 13:06:58 +0100

    add Jacobi-matrix in cerf_hypothesis.m

--- functions/test/cerf_hypothesis.m
+++ functions/test/cerf_hypothesis.m
@@ -13,13 +13,18 @@ k2 = theta(3);
 h = f*exp(-k1*t)+(1-f)*exp(-k2*t);            % Cerf model
 
 if nargout > 1
-	dh = -k1*f*exp(-k1*t)-(1-f)*k2*exp(-k2*t);  % derivation of Cerf model
-	
 % 	dh ==> element(mxn)
 % 	| dh_1/dtheta_1 dh_1/dtheta_2 dh_1/dtheta_2 |
 % 	| dh_2/dtheta_1 dh_2/dtheta_2 dh_2/dtheta_2 |
 % 	| dh_.../dtheta_1 dh_.../dtheta_2 dh_.../dtheta_2 |
 % 	| dh_m/dtheta_1 dh_m/dtheta_2 dh_m/dtheta_2 |
+
+dh_dtheta1 = exp(-k1*t)-exp(-k2*t);
+dh_dtheta2 = -f*t.*exp(-k1*t);
+dh_dtheta3 = t.*exp(-k2*t)*(f-1);
+
+dh = [dh_dtheta1, dh_dtheta2, dh_dtheta3];  % Jacobi matrix of Cerf model
+
 end
 
 end
