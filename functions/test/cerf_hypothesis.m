d470e8cac713432e5ae96b5ef7dc88b48d153cb8
Digusil <pircher.th@googlemail.com>
2015-12-17 11:16:12 +0100

    cerf derivation to jacobi

--- functions/test/cerf_hypothesis.m
+++ functions/test/cerf_hypothesis.m
@@ -12,6 +12,14 @@ k2 = theta(3);
 
 h = f*exp(-k1*t)+(1-f)*exp(-k2*t);            % Cerf model
 
-dh = -k1*f*exp(-k1*t)-(1-f)*k2*exp(-k2*t);  % derivation of Cerf model
+if nargout > 1
+	dh = -k1*f*exp(-k1*t)-(1-f)*k2*exp(-k2*t);  % derivation of Cerf model
+	
+% 	dh ==> element(mxn)
+% 	| dh_1/dtheta_1 dh_1/dtheta_2 dh_1/dtheta_2 |
+% 	| dh_2/dtheta_1 dh_2/dtheta_2 dh_2/dtheta_2 |
+% 	| dh_.../dtheta_1 dh_.../dtheta_2 dh_.../dtheta_2 |
+% 	| dh_m/dtheta_1 dh_m/dtheta_2 dh_m/dtheta_2 |
+end
 
 end
