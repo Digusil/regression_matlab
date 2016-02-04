6e685e2544733d55aec0a51d3de12bcbf6f0c609
Digusil <pircher.th@googlemail.com>
2016-02-04 16:01:00 +0100

    mod clacR2.m

--- functions/calcR2.m
+++ functions/calcR2.m
@@ -1,8 +1,8 @@
 function rSquare = calcR2(y_appr, y_true)
-    y_mean = mean(y_true);
-    
-    tmp1 = y_true - y_appr;
-    tmp2 = y_true - y_mean;
-    
-    rSquare = 1- (tmp1'*tmp1)/(tmp2'*tmp2);
+	y_mean = mean(y_true,1);
+	
+	tmp1 = y_true - y_appr;
+	tmp2 = y_true - ones(size(y_true,1),1)*y_mean;
+	
+	rSquare = 1- (diag(tmp1'*tmp1))./diag((tmp2'*tmp2));
 end
