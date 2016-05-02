d2562489e892e63056a099d193c3096981ccb3aa
vy03ravo <ninherrmann@web.de>
2016-05-02 13:58:04 +0200

    optimisation for logarithmic h_cerf

--- functions/train.m
+++ functions/train.m
@@ -3,11 +3,14 @@ function [theta, J, flag] = train(inputs, targets, hypothesis, lambda, theta0, o
 
 	options_tmp = optimset('Display','off');
 	options = optimset(options_tmp, options);
-	
-	[theta, J, flag] = fminunc(@(theta) costfunction(inputs, targets, theta, hypothesis, lambda),theta0, options);
-	
+
+	%[theta, J, flag] = fminunc(@(theta) costfunction(inputs, targets, theta, hypothesis, lambda),theta0, options);
+
+	% optimisation for cerf hypothesis (log-function)
+	[theta, J, flag] = fmincon(@(theta) costfunction(inputs, targets, theta, hypothesis, lambda),theta0, [],[],[],[],[0;-Inf;-Inf],[1;Inf;Inf], [], options);
+
 	if flag == 0 && strcmp(options.Display, 'off')
 		warning('Optimisation:Iterations', 'Optimisation exeeds maximum number of iterations!')
 	end
-	
+
 end
