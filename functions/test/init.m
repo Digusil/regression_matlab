a3e72bef76f742513109f8e03848ec772d194f85
Digusil <pircher.th@googlemail.com>
2016-06-12 18:36:58 +0200

    clean directory

--- functions/test/init.m
+++
@@ -1,3 +0,0 @@
-if ~exist('calcR2.m','file')	% check if the path is already added
-	addpath('..');
-end
--- functions/test/numRegdFoo.m
+++
@@ -1,4 +0,0 @@
-%% numRegFoo
-function [dy] = numRegdFoo(t,y,c)
-	dy = -c(1)*(1-(c(2)./y).^c(3)).*y;
-end
