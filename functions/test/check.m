e705e3d3885333969be8c70a54d9490b8912729f
Digusil <pircher.th@googlemail.com>
2016-07-06 22:22:46 +0200

    show check result independently from the result flag

--- functions/test/check.m
+++ functions/test/check.m
@@ -7,23 +7,29 @@ function [check_results] = check(checknames, expacted_values, check_values)
 
 		for i = 1:n
 			check_results(i) = isequal(expacted_values{i}, check_values{i});
+			disp_check(checknames{i}, check_results(i))
 		end
 
 	else
 		check_results = isequal(expacted_values, check_values);
+		disp_check(checknames, check_results)
 	end
 
 	if nargout == 0
-		if iscell(check_results)
-			for i = 1:n
-				disp_check(checknames{i}, check_results(i))
-			end
-		else
-			disp_check(checknames, check_results)
-		end
-
 		clear check_results
 	end
+
+%	if nargout == 0
+%		if iscell(check_results)
+%			for i = 1:n
+%				disp_check(checknames{i}, check_results(i))
+%			end
+%		else
+%			disp_check(checknames, check_results)
+%		end
+%
+%		clear check_results
+%	end
 
 end
 
