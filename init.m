32b856149da663fc6383f7cdcab833812ddcbcbe
Digusil <pircher.th@googlemail.com>
2016-07-22 14:39:31 +0200

    clear pathstring after addpath

--- init.m
+++ init.m
@@ -1,5 +1,6 @@
 %% init: add needed directories to path
 if ~exist('linReg.m','file')	% check if the path is already added
-	[pathstr,name,ext] = fileparts(mfilename('fullpath')); 	% get path of the init-file
+	[pathstr,~,~] = fileparts(mfilename('fullpath')); 		% get path of the init-file
 	addpath(fullfile(pathstr, 'functions'));				% add path
+	clear pathstr
 end
